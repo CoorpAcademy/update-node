ada12983b9c1b1cc2560317a2a0374f520fe5f65
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-04-02 09:31:32 +0200

    Rework executeScript that was buggy

--- src/core/script.js
+++ src/core/script.js
@@ -3,19 +3,15 @@ const shelljs = require('shelljs');
 const c = require('chalk');
 
 const executeScript = commands =>
-  Promise.reduce(
-    commands,
-    (acc, cmd) => {
-      if (!cmd) return;
-      return new Promise((resolve, reject) => {
-        const child = shelljs.exec(cmd, {async: true, silent: true}, err => {
-          if (err) return reject(err);
-          resolve();
-        });
-        child.stdout.on('data', data => process.stdout.write(c.dim(data)));
+  Promise.mapSeries(commands, cmd => {
+    if (!cmd) return;
+    return new Promise((resolve, reject) => {
+      const child = shelljs.exec(cmd, {async: true, silent: true}, err => {
+        if (err) return reject(err);
+        resolve();
       });
-    },
-    null
-  );
+      child.stdout.on('data', data => process.stdout.write(c.dim(data)));
+    });
+  });
 
 module.exports = executeScript;
