340a061f539a05c86802165cd6ad6258e54b0400
Adriean Khisbe <adriean.khisbe@live.fr>
2019-04-03 09:36:47 +0200

    Introduce functions to get latest package version

---
+++ src/core/versions.js
@@ -0,0 +1,30 @@
+const shelljs = require('shelljs');
+const _ = require('lodash/fp');
+const semver = require('semver');
+// const Promise = require('bluebird');
+
+
+const EXACT_PREFIX = '';
+
+const MINOR_PREFIX = '^';
+const PATCH_PREFIX = '~';
+
+const versionsForPackage = async (pkg, includePrerelease = false) => {
+  const pkgVersions = await new Promise((resolve, reject) => {
+    const versionsRaw = shelljs.exec(`npm view ${pkg} versions --json`, {silent: true});
+    try {
+      resolve(JSON.parse(versionsRaw.stdout));
+    } catch (err) {
+      return reject(err);
+    }
+  });
+  return includePrerelease ? pkgVersions : _.filter(v => !semver.prerelease(v), pkgVersions);
+};
+
+const latestVersionForPackage = async (pkg, includePrerelease = false) => {
+    return _.last(await versionsForPackage(pkg, includePrerelease))
+};
+
+module.exports = {
+    EXACT_PREFIX, MINOR_PREFIX, PATCH_PREFIX, versionsForPackage, latestVersionForPackage
+}
--- src/updatees/package.js
+++ src/updatees/package.js
@@ -2,13 +2,11 @@ const path = require('path');
 const fs = require('fs');
 const _ = require('lodash/fp');
 const Promise = require('bluebird');
+const {EXACT_PREFIX, MINOR_PREFIX} = require('../core/versions');
 
 const writeFile = Promise.promisify(fs.writeFile);
 const readFile = Promise.promisify(fs.readFile);
 
-const EXACT_PREFIX = '';
-
-const MINOR_PREFIX = '^';
 const readPackage = packagePath => {
   return readFile(packagePath, 'utf8').then(JSON.parse);
 };
@@ -24,6 +22,7 @@ const updatePackage = (node, npm, pkg, exact = false) => {
   const newPackageP = packageP
     .then(node ? _.set('engines.node', `${SAVE_PREFIX}${node}`) : _.identity)
     .then(npm ? _.set('engines.npm', `${SAVE_PREFIX}${npm}`) : _.identity);
+  // FIXME: improve, preser >=
 
   return newPackageP
     .then(obj => {
