e848bdc91512ae4cbc8d7ac3ecf95551d6710e85
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-08-19 14:42:26 +0200

    Preserve format patching travis using yaml low level CST parser

--- src/updatees/travis.js
+++ src/updatees/travis.js
@@ -8,17 +8,31 @@ const YAML = require('yaml');
 const readFile = Promise.promisify(fs.readFile);
 const writeFile = Promise.promisify(fs.writeFile);
 
-const updateTravis = (node, travis) => {
-  if (_.isArray(travis)) return Promise.map(travis, t => updateTravis(node, t));
+const patchVersionInTravisYaml = nodeVersion => yamlString => {
+  const yamlCst = YAML.parseCST(yamlString);
+  const documentItems = yamlCst[0].contents[0].items;
+  const nodejsIndex = _.findIndex(
+    _.pipe(
+      _.get('strValue'),
+      _.equals('node_js')
+    ),
+    documentItems
+  );
+  // patch the first item of list in cst node behind node_js
+  documentItems[nodejsIndex + 1].node.items[0].node.value = nodeVersion;
+  return yamlCst.toString();
+};
 
-  if (!travis || !node) return Promise.resolve();
+const updateTravis = (nodeVersion, travis) => {
+  if (_.isArray(travis)) return Promise.map(travis, t => updateTravis(nodeVersion, t));
+
+  if (!travis || !nodeVersion) return Promise.resolve();
 
   return readFile(travis, 'utf8')
-    .then(content => YAML.parseDocument(content, {merge: true}))
-    .tap(yaml => yaml.setIn(['node_js', 0], node))
-    .then(yaml => yaml.toString())
+    .then(patchVersionInTravisYaml(nodeVersion))
     .then(newTravisYaml => writeFile(travis, newTravisYaml, 'utf8'))
     .tap(() => process.stdout.write(`- Write ${c.bold.dim(path.basename(travis))}\n`));
 };
 
 module.exports = updateTravis;
+module.exports.patchVersionInTravisYaml = patchVersionInTravisYaml;
