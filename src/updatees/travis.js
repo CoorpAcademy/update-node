46ddff7b715ac968349f08eed4ccde84c70d87cd
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-08-19 17:23:53 +0200

    Explicit error message when travis config don't have expected structure

--- src/updatees/travis.js
+++ src/updatees/travis.js
@@ -10,7 +10,8 @@ const writeFile = Promise.promisify(fs.writeFile);
 
 const patchVersionInTravisYaml = nodeVersion => yamlString => {
   const yamlCst = YAML.parseCST(yamlString);
-  const documentItems = yamlCst[0].contents[0].items;
+  const documentItems = _.get('[0].contents[0].items', yamlCst);
+  if (!documentItems) throw new Error('Travis config file seems not be a valid yaml');
   const nodejsIndex = _.findIndex(
     _.pipe(
       _.get('strValue'),
@@ -18,6 +19,7 @@ const patchVersionInTravisYaml = nodeVersion => yamlString => {
     ),
     documentItems
   );
+  if (nodejsIndex === -1) throw new Error('Travis config has missing node_js key');
   // patch the first item of list in cst node behind node_js
   const currentVersionRange = documentItems[nodejsIndex + 1].node.items[0].node.range;
   return (
