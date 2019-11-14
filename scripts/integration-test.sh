dbf95ad255763e4d9082b451b46ea5005114f00f
Adriean Khisbe <adriean.khisbe@live.fr>
2019-11-14 14:17:51 +0100

    Integrate integration-test into local workflow :trackball:

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 set -e
+echo "> Linking update-node"
 npm link
+echo
+
 cd test/integration
+if [ -d .git ]; then
+    echo "> Reset integration folder"
+    git reset --hard init || true
+    rm -rf .git
+fi
+
+echo "> Setting up integration folder"
 git init
 git add .
 git commit -m "Initial dummy commit"
+git tag init
 npm install
+echo "> Install update node"
 npm install --save-dev @coorpacademy/update-node
-git add package{,-lock}.json
+git add package.json package-lock.json
 git commit -m "Install update-node"
+
+echo "> Running update-node commands"
 npm run update -- upgrade --local
-git log --graph --decorate --pretty=oneline --abbrev-commit
+git --no-pager log --graph --decorate --pretty=oneline --abbrev-commit
 npm run update -- auto-bump --local
-git log --graph --decorate --pretty=oneline --abbrev-commit
+git --no-pager log --graph --decorate --pretty=oneline --abbrev-commit
+
+git checkout  -b origins init
