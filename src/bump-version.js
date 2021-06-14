bfc228cecca2eafc41d568dfe16d81c6704fc712
AdrieanKhisbe <adriean.khisbe@live.fr>
2021-06-14 16:02:29 +0200

    Introduce a keyword customizable release selection system :open_book:

--- src/bump-version.js
+++ src/bump-version.js
@@ -3,6 +3,7 @@
 const childProcess = require('child_process');
 const c = require('chalk');
 const _ = require('lodash/fp');
+const minimatch = require('minimatch');
 const {headClean, headMessage, pushFiles} = require('./core/git');
 const {makeError} = require('./core/utils');
 const executeScript = require('./core/script');
@@ -10,24 +11,51 @@ const executeScript = require('./core/script');
 const MAJOR = 'major';
 const MINOR = 'minor';
 const PATCH = 'patch';
-const NOOP = null;
+const NOOP = 'noop';
 
-const builtInSelectReleaseType = message => {
-  const firstLine = message.split('\n')[0];
-  const containsPatchKeyWords = /bug|fix|tweak|plugging/i.test(firstLine);
-  const tooShortMessage = firstLine.split(' ').length < 5; // 4 word + PR tag
-  const hashtagMinor = /#minor\b/i.test(firstLine);
-  const hashtagBug = /#bug\b/i.test(firstLine);
-  const hashtagNoop = /#no(op|no[_-]?realease)\b/i.test(message);
-  const containsMinorKeyWords = /release|feature/i.test(firstLine);
-  const isSquashOrMerge = /Merge pull request #\d+|\(#\D+\)/.test(firstLine);
+const DEFAULT_KEYWORDS = {
+  noop: ['#noop', '#no+(_|-|)release'],
+  major: '#major',
+  minor: ['#minor'],
+  patch: ['#bug', '#fix', '#tweak', 'plugging', '#updates']
+};
+const DEFAULT_KEYWORD_PRIORITY = [NOOP, MAJOR, PATCH, MINOR];
 
-  if (hashtagNoop) return NOOP;
-  if (hashtagBug || containsPatchKeyWords) return PATCH;
-  if (hashtagMinor || containsMinorKeyWords) return MINOR;
-  if (!isSquashOrMerge) return MINOR;
-  if (tooShortMessage) return PATCH;
-  return MINOR;
+const builtInSelectReleaseType = (config, message) => {
+  const {keywords, releasePriority} = config;
+
+  for (const releaseType of releasePriority) {
+    const releaseTypeConfig = keywords[releaseType];
+    if (!releaseTypeConfig) continue;
+    if (releaseTypeConfig === true) return releaseType;
+
+    const patterns = _.flatten([releaseTypeConfig]);
+    if (_.some(pattern => minimatch(_.toLower(message), _.toLower(`*${pattern}*`)), patterns))
+      return releaseType;
+  }
+  return _.last(releasePriority);
+};
+
+const mergeCustomKeywordsWithDefault = customKeywords => {
+  if (!customKeywords) return DEFAULT_KEYWORDS;
+  return _.pipe(
+    _.toPairs,
+    _.map(([releaseType, defaultKeywords]) => [
+      releaseType,
+      _.concat(defaultKeywords, _.flatten([_.getOr([], releaseType, customKeywords)]))
+    ]),
+    _.fromPairs
+  )(DEFAULT_KEYWORDS);
+};
+
+const getBuiltInSelection = async (config, messageOverride) => {
+  const message = messageOverride || (await headMessage());
+
+  const releasePriority = _.getOr(DEFAULT_KEYWORD_PRIORITY, 'priority', config);
+  const keywords =
+    _.get('keywords', config) || mergeCustomKeywordsWithDefault(_.get('custom-keywords', config));
+
+  return builtInSelectReleaseType({keywords, releasePriority}, message);
 };
 
 const getCustomSelection = cmd => {
@@ -41,16 +69,17 @@ const getCustomSelection = cmd => {
   }
 };
 
-const getReleaseType = async selectionCommand => {
-  const releaseType = selectionCommand
-    ? getCustomSelection(selectionCommand)
-    : await builtInSelectReleaseType(await headMessage());
+const getReleaseType = async relaseConfig => {
+  const releaseSelector = relaseConfig['release-type-command'];
+  const releaseType = releaseSelector
+    ? getCustomSelection(releaseSelector)
+    : await getBuiltInSelection(relaseConfig);
   if (!_.includes(releaseType, [MAJOR, MINOR, PATCH, NOOP]))
     throw makeError(`Invalid release type ${releaseType}`);
   return releaseType;
 };
 
-module.exports = async config => {
+const main = async config => {
   if (!(await headClean())) throw makeError('Not a clean state', {exitCode: 4});
   const autoBumpConfig = config['auto-bump'];
   if (_.isBoolean(autoBumpConfig)) {
@@ -59,10 +88,9 @@ module.exports = async config => {
       return;
     }
   } else if (_.isEmpty(autoBumpConfig)) throw makeError('No Config for autobump', {exitCode: 3});
-  const releaseSelector = autoBumpConfig['release-type-command'];
-  const releaseType = await getReleaseType(releaseSelector);
+  const releaseType = await getReleaseType(autoBumpConfig);
 
-  if (!releaseType) return process.stdout.write(`Won't make a release\n`);
+  if (releaseType === NOOP) return process.stdout.write(`Won't make a release\n`);
 
   process.stdout.write(c.bold.yellow(`About to make a ${c.bold.blue(releaseType)} release\n`));
   const bumpVersionCommand = autoBumpConfig['bump-command'] || 'npm version -m "v%s"';
@@ -94,4 +122,13 @@ module.exports = async config => {
     ]);
     process.stdout.write(c.bold.green(`Successfully merged branch ${branch}\n`));
   }
+};
+
+module.exports = {
+  main,
+  builtInSelectReleaseType,
+  getReleaseType,
+  getBuiltInSelection,
+  getCustomSelection,
+  mergeCustomKeywordsWithDefault
 };
